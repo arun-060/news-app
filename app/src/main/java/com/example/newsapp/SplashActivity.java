6a9adbb330bc7b71e2b79bb69ce1065a36bf19df
arun-060 <guptaarunkumar060@gmail.com>
2024-01-27 20:22:52 +0530

    adding animation to splash screen

--- app/src/main/java/com/example/newsapp/SplashActivity.java
+++ app/src/main/java/com/example/newsapp/SplashActivity.java
@@ -1,19 +1,41 @@
 package com.example.newsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static final long SPLASH_DELAY = 1500;
+    private static final long SPLASH_DELAY = 3000;
+
+    Animation topAnim, bottomAnim;
+
+    ImageView image;
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image = findViewById(R.id.image);
+        name = findViewById(R.id.name);
+
+        image.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
